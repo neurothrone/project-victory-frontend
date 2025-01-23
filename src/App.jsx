8f8f7f6f7df3c929bcac9d1b5cf0e6132fc23030
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-01-23 16:16:00 +0100

    add socket package and implement listener

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-// import './App.css'
-
 import Header from "./components/Header.jsx";
 import ChatWindow from "./components/ChatWindow.jsx";
 import InputGroup from "./components/InputGroup.jsx";
 import {useEffect, useState} from "react";
+import {io} from "socket.io-client";
 
 function App() {
   const [messages, setMessages] = useState([]);
+
+  const socket = io("https://project-victory.azurewebsites.net/api/messages", {
+    autoConnect: true
+  });
+
+  socket.on("message", (message) => {
+    console.log("New message received:", message);
+    setMessages((prevMessages) => [...prevMessages, message]);
+  });
 
   useEffect(() => {
     async function fetchMessages() {
       await loadMessages();
     }
+
     fetchMessages();
   }, []);
-
-
 
   async function onMessageReceived(message) {
     try {
