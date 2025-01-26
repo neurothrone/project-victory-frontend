f0cee0bdf883b3c248b3f3f85ee20265338bc0ed
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-01-26 01:45:29 +0100

    add logic and styling for currently logged in user's username

--- src/pages/Chat/components/Message.jsx
+++ src/pages/Chat/components/Message.jsx
@@ -2,10 +2,15 @@
   // eslint-disable-next-line react/prop-types
   const {message} = props;
 
+  const currentUsername = localStorage.getItem("username");
+
   return (
     <div className="message">
       <div className="message-header">
-        <span className="username">{message.username}</span>
+        <span
+          className={currentUsername === message.username ? "current-username" : "username"}>
+          {message.username}
+        </span>
         <span className="timestamp">{new Date(message.timestamp).toLocaleString("en-US", {
           hour: "2-digit",
           minute: "2-digit",
