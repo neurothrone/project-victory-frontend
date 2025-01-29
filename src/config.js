77375cda083d9bc543598e2c2a0b26467b910bdf
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-01-29 03:31:34 +0100

    hardcode prod url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 //const BASE_API_URL = import.meta.env.VITE_DEV_API_BASE_URL;
-const BASE_API_URL = import.meta.env.VITE_PROD_API_BASE_URL;
+// const BASE_API_URL = import.meta.env.VITE_PROD_API_BASE_URL;
+const BASE_API_URL = "https://project-victory-backend.azurewebsites.net";
 
 export {BASE_API_URL};
